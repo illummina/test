b2b56db49c1370f22d9b0a4fc206f1f7036926d9
illummina <anuta9612@gmail.com>
2023-09-28 17:25:29 +0200

    update nibiru

--- nibiru.sh
+++ nibiru.sh
@@ -1,59 +1,76 @@
 #!/bin/bash
 
 is_valid_moniker() {
-  local moniker="$1"
-  if [[ "$moniker" =~ ^[A-Z][A-Za-z0-9]*$ && ${#moniker} -le 15 ]]; then
-    return 0
-  else
-    return 1
-  fi
+    local moniker="$1"
+    if [[ "$moniker" =~ ^[A-Z][A-Za-z0-9]*$ && ${#moniker} -le 15 ]]; then
+        return 0  # Valid moniker
+    else
+        return 1  # Invalid moniker
+    fi
 }
 
 sudo apt update && sudo apt upgrade --yes
 
 NIBIRU_VERSION=$(curl -s "https://github.com/NibiruChain/nibiru/releases" | grep -oE "v[0-9]+\.[0-9]+\.[0-9]+" | head -n 1)
-
 curl -s "https://get.nibiru.fi/@${NIBIRU_VERSION}!" | bash
 
-#TODO Make version selectable
-#NIBIRU_INSTALLED_VERSION=$(nibid version)
-#echo "$NIBIRU_INSTALLED_VERSION"
-#if [[ "v$NIBIRU_INSTALLED_VERSION" != "$NIBIRU_VERSION" ]]; then
-#    echo "WARNING: Nibiru version ($NIBIRU_INSTALLED_VERSION) does not match the installed version ($NIBIRU_VERSION)"
-#fi
 nibid version
 
 while true; do
-  read -p "Enter a moniker name (starts with an uppercase letter, contains only letters and numbers, not longer than 15 characters): " moniker
+    read -p "Enter a moniker name (starts with an uppercase letter, contains only letters and numbers, not longer than 15 characters): " moniker
 
-  if is_valid_moniker "$moniker"; then
-    nibid init "$moniker" --chain-id=nibiru-itn-2 --home $HOME/.nibid
-    break
-  else
-    echo "Invalid moniker name. Please follow the naming rules."
-  fi
+    if is_valid_moniker "$moniker"; then
+        nibid init "$moniker" --chain-id=nibiru-itn-2 --home $HOME/.nibid
+        break
+    else
+        echo "Invalid moniker name. Please follow the naming rules."
+    fi
 done
 
-# TODO: selectable from existing wallet or create new
-nibid keys add wallet --recover
-##Команда запросит указать пароль, а затем выдаст мнемоник фразу, которую нужно будет обязательно сохранить в безопасное место.
-##nibid keys add <key-name>
-
-NETWORK=nibiru-itn-2
-curl -s "https://networks.itn2.nibiru.fi/$NETWORK/genesis" > $HOME/.nibid/config/genesis.json
-
-shasum -a 256 $HOME/.nibid/config/genesis.json
-
-sed -i 's|seeds =.*|seeds = "'$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/seeds)'"|g' $HOME/.nibid/config/config.toml
-
-sed -i 's/minimum-gas-prices =.*/minimum-gas-prices = "0.025unibi"/g' $HOME/.nibid/config/app.toml
-
-config_file="$HOME/.nibid/config/config.toml"
-sed -i "s|enable =.*|enable = true|g" "$config_file"
-sed -i "s|rpc_servers =.*|rpc_servers = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/rpc_servers)\"|g" "$config_file"
-sed -i "s|trust_height =.*|trust_height = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/trust_height)\"|g" "$config_file"
-sed -i "s|trust_hash =.*|trust_hash = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/trust_hash)\"|g" "$config_file"
-
-sudo systemctl start nibiru
+## 5. Create a local key pair (Change <key-name> as needed)
+#nibid keys add <key-name>
+#
+## 6. Fetch and copy the genesis file
+#NETWORK=nibiru-itn-2
+#curl -s "https://networks.itn2.nibiru.fi/$NETWORK/genesis" > $HOME/.nibid/config/genesis.json
+#
+## 7. (Optional) Verify Genesis File Checksum
+#shasum -a 256 $HOME/.nibid/config/genesis.json
+#
+## 8. Update seeds list in the configuration file
+#sed -i 's|seeds =.*|seeds = "'$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/seeds)'"|g' $HOME/.nibid/config/config.toml
+#
+## 9. Set minimum gas prices
+#sed -i 's/minimum-gas-prices =.*/minimum-gas-prices = "0.025unibi"/g' $HOME/.nibid/config/app.toml
+#
+## 10. Setup state-sync parameters
+#config_file="$HOME/.nibid/config/config.toml"
+#sed -i "s|enable =.*|enable = true|g" "$config_file"
+#sed -i "s|rpc_servers =.*|rpc_servers = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/rpc_servers)\"|g" "$config_file"
+#sed -i "s|trust_height =.*|trust_height = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/trust_height)\"|g" "$config_file"
+#sed -i "s|trust_hash =.*|trust_hash = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/trust_hash)\"|g" "$config_file"
+#
+## 11. Create and enable the systemd service unit file
+#SERVICE_UNIT_FILE="/etc/systemd/system/nibiru.service"
+#sudo bash -c "cat > $SERVICE_UNIT_FILE" <<EOL
+#[Unit]
+#Description=Nibiru Node
+#
+#[Service]
+#ExecStart=$(which nibid) start
+#Restart=on-failure
+#User=$USER
+#Group=$USER
+#
+#[Install]
+#WantedBy=multi-user.target
+#EOL
+#
+## Reload systemd configuration
+#sudo systemctl daemon-reload
+#
+## Enable and start the Nibiru service
+#sudo systemctl enable nibiru.service
+#sudo systemctl start nibiru.service
 
 echo "Nibiru setup completed."
--- nibiru_old.sh
+++ nibiru_old.sh
@@ -1,64 +1,63 @@
 #!/bin/bash
 
-# name validator
 is_valid_moniker() {
-    local moniker="$1"
-    if [[ "$moniker" =~ ^[A-Z][A-Za-z0-9]*$ && ${#moniker} -le 15 ]]; then
-        return 0
-    else
-        return 1
-    fi
+  local moniker="$1"
+  if [[ "$moniker" =~ ^[A-Z][A-Za-z0-9]*$ && ${#moniker} -le 15 ]]; then
+    return 0
+  else
+    return 1
+  fi
 }
-#
-sudo apt update
-sudo apt upgrade --yes
+
+sudo apt update && sudo apt upgrade --yes
 
 NIBIRU_VERSION=$(curl -s "https://github.com/NibiruChain/nibiru/releases" | grep -oE "v[0-9]+\.[0-9]+\.[0-9]+" | head -n 1)
 
-NIBIRU_INSTALL_SCRIPT="https://get.nibiru.fi/@${NIBIRU_VERSION}!"
-curl -s "$NIBIRU_INSTALL_SCRIPT" | bash
+curl -s "https://get.nibiru.fi/@${NIBIRU_VERSION}!" | bash
 
-NIBIRU_INSTALLED_VERSION=$(nibid version)
-echo "$NIBIRU_INSTALLED_VERSION"
-if [[ "v$NIBIRU_INSTALLED_VERSION" != "$NIBIRU_VERSION" ]]; then
-    echo "WARNING: Nibiru version ($NIBIRU_INSTALLED_VERSION) does not match the installed version ($NIBIRU_VERSION)"
-fi
+#TODO Make version selectable
+#NIBIRU_INSTALLED_VERSION=$(nibid version)
+#echo "$NIBIRU_INSTALLED_VERSION"
+#if [[ "v$NIBIRU_INSTALLED_VERSION" != "$NIBIRU_VERSION" ]]; then
+#    echo "WARNING: Nibiru version ($NIBIRU_INSTALLED_VERSION) does not match the installed version ($NIBIRU_VERSION)"
+#fi
+nibid version
 
 while true; do
-  read -p "Enter the moniker name (starts with an uppercase letter, contains only letters and numbers, not longer than 15 characters): " moniker
+  read -p "Enter a moniker name (starts with an uppercase letter, contains only letters and numbers, not longer than 15 characters): " moniker
 
   if is_valid_moniker "$moniker"; then
-      nibid init "$moniker" --chain-id=nibiru-itn-2 --home $HOME/.nibid
-      nibid keys add wallet
-
-      NETWORK=nibiru-itn-2
-      curl -s https://networks.itn2.nibiru.fi/$NETWORK/genesis > $HOME/.nibid/config/genesis.json
-
-      # verify checksum
-      shasum -a 256 $HOME/.nibid/config/genesis.json
-
-      config_file="$HOME/.nibid/config/config.toml"
-      sed -i 's|seeds =.*|seeds = "'$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/seeds)'"|g' $config_file
-
-      cat <<EOF | sudo tee /etc/systemd/system/nibiru.service
-[Unit]
-Description=Nibiru Node
-After=network-online.target
-
-[Service]
-ExecStart=$(which nibid) start
-Restart=on-failure
-RestartSec=3
-
-[Install]
-WantedBy=multi-user.target
-EOF
-
-    systemctl daemon-reload
-    systemctl start nibiru
-    systemctl enable nibiru
+    nibid init "$moniker" --chain-id=nibiru-itn-2 --home $HOME/.nibid
     break
   else
-    echo "Invalid moniker name. Please use ONLY letters and numbers"
+    echo "Invalid moniker name. Please follow the naming rules."
   fi
 done
+
+# TODO: selectable from existing wallet or create new
+nibid keys add wallet --recover
+##Команда запросит указать пароль, а затем выдаст мнемоник фразу, которую нужно будет обязательно сохранить в безопасное место.
+##nibid keys add <key-name>
+
+NETWORK=nibiru-itn-2
+curl -s "https://networks.itn2.nibiru.fi/$NETWORK/genesis" > $HOME/.nibid/config/genesis.json
+
+shasum -a 256 $HOME/.nibid/config/genesis.json
+
+sed -i 's|seeds =.*|seeds = "'$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/seeds)'"|g' $HOME/.nibid/config/config.toml
+
+sed -i 's/minimum-gas-prices =.*/minimum-gas-prices = "0.025unibi"/g' $HOME/.nibid/config/app.toml
+
+config_file="$HOME/.nibid/config/config.toml"
+sed -i "s|enable =.*|enable = true|g" "$config_file"
+sed -i "s|rpc_servers =.*|rpc_servers = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/rpc_servers)\"|g" "$config_file"
+sed -i "s|trust_height =.*|trust_height = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/trust_height)\"|g" "$config_file"
+sed -i "s|trust_hash =.*|trust_hash = \"$(curl -s https://networks.itn2.nibiru.fi/$NETWORK/trust_hash)\"|g" "$config_file"
+
+sudo systemctl start nibiru
+
+echo "Nibiru setup completed."
+
+
+
+
